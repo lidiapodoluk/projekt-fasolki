bb71e4bd012f6fb609c00601b7aeab3cb6a454cb
Lidia Podoluk <lidiapodoluk@gmail.com>
2024-06-20 23:46:40 +0200

    new file karty.py added

---
+++ karty.py
@@ -0,0 +1,55 @@
+from Karta import *
+
+inf = 100
+
+Nazwa = {
+    0   :   "Ogrodnik",
+    1   :   "Golas",
+    2   :   "Bokser",
+    3   :   "Hipis",
+    4   :   "Pijak",
+    5   :   "Grubas",
+    6   :   "Piroman",
+    7   :   "Kowboj"
+}
+
+Wartosc = {
+    0   :   [0, 1.5, 2, 3, inf],
+    1   :   [0, 2, 3, 4, 5],
+    2   :   [0, 2, 4, 5, 6],
+    3   :   [0, 2, 4, 6, 7],
+    4   :   [0, 3, 5, 6, 7],
+    5   :   [0, 3, 5, 7, 8],
+    6   :   [0, 3, 6, 8, 9],
+    7   :   [0, 4, 6, 8, 10],
+}
+
+Ilosc = {
+    0   :   6,
+    1   :   8,
+    2   :   10,
+    3   :   12,
+    4   :   14,
+    5   :   16,
+    6   :   18,
+    7   :   20
+}
+
+#if __name__ == "__main__":
+rodzaje_kart = []
+karty = []
+for i in range (8):
+    nazwa, wartosc, ilosc = Nazwa[i], Wartosc[i], Ilosc[i]
+    karta = Karta(nazwa, wartosc, ilosc)
+    #print(karta)
+    rodzaje_kart.append(karta)
+    karty.extend([karta] * ilosc)
+
+
+#test
+'''
+for karta in rodzaje_kart:
+    print (karta)
+for karta in karty:
+    print(karta)
+'''
