dd467753f5cff14bc29525a204ee68d213b9cfdc
Lidia Podoluk <lidiapodoluk@gmail.com>
2024-06-22 18:40:26 +0200

    almost completed trading implementation for the user

--- Akcja.py
+++ Akcja.py
@@ -18,18 +18,14 @@ class Akcja(ABC):
 
 
 class SadzFasoleZReki(Akcja):
-    '''
-    def __init__(self, player, karta, pole_index):
-        super().__init__(player)
-        self.karta = karta
-        self.pole_index = pole_index
-    '''
-
+    
     def __init__(self, player):
         super().__init__(player)
 
     def wykonaj(self):
         try:
+            if not len(self.player.karty_na_rece):
+                raise Exception("Error! Gracz nie ma kart na rece")  
             karta = self.player.karty_na_rece.pop(0)
             print ("Na ktorym polu? (0/1):   ", end='')
             wybrane_pole = int(input())
@@ -46,7 +42,29 @@ class SadzFasoleZReki(Akcja):
                 print(p, end=', ')
             print()
             print("     przed")
-        except not len(self.player.karty_na_rece):
-            print("Error! Gracz nie ma kart na rece")  
+        except Exception:
+            raise
 
 
+class SadzFasoleZHandlu(Akcja):
+    
+    def __init__(self, player, karta):
+        super().__init__(player)
+        self.karta = karta
+
+    def wykonaj(self):
+        print ("Na ktorym polu chcesz posadzic karte  ", self.karta.rodzaj_fasolki, " ? (0/1):   ", end='')
+        wybrane_pole = int(input())
+        pole = self.player.pola[wybrane_pole]
+        if not pole.rodzaj_fasolki == self.karta:
+            #akcja = ZbierzZbiory(player, pole, ilosc)
+            #akcja.wykonaj()
+            pole.zbierz_zbiory(pole.ilosc)
+        pole.sadz_fasolki(self.karta)
+
+        print("     przed")
+        print("Pola gracza po zasadzeniu:  ", end='')
+        for p in self.player.pola:
+            print(p, end=', ')
+        print()
+        print("     przed")
