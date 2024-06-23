a0b1b0d803ab8eccf731393e16c30317577ae240
Lidia Podoluk <lidiapodoluk@gmail.com>
2024-06-24 01:51:45 +0200

    some useful numbers etc

---
+++ zmienne.py
@@ -0,0 +1,15 @@
+import screeninfo
+import pygame
+
+#Kolory
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+LIGHT_BLUE = (179, 236, 255)
+BLACK = (0, 0, 0)
+
+#Wymiary
+SCREEN = screeninfo.get_monitors()[0]
+SCREEN_WIDTH = SCREEN.width
+SCREEN_HEIGHT = SCREEN.height
+CARD_SIZE = (129, 200)
+POLE_SIZE = (388, 200)
